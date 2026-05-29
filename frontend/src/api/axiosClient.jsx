99d87666714bb69869f032134e30e7afaea1ef0b
annguyen <ntri337@gmail.com>
2026-05-29 13:04:38 +0700

    fix: connect to render api production

--- frontend/src/api/axiosClient.jsx
+++ frontend/src/api/axiosClient.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-lap23.onrender.com/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
